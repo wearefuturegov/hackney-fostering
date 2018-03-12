f4abac1a782efb8f3fce9cd835fab298a14b880c
pezholio <pezholio@gmail.com>
2018-03-12 19:38:34 +0000

    Fix Rubocop

--- app/rules/employment_history_rule.rb
+++ app/rules/employment_history_rule.rb
@@ -1,7 +1,5 @@
 class EmploymentHistoryRule < Incredible::Rule
-
   def process
     @next_step = params[:application][:employment_type] == 'unemployed' ? :address : :national_insurance
   end
-
 end
