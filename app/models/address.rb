35e162a57f1f24df16deafa5648d2367f940267c
pezholio <pezholio@gmail.com>
2018-03-13 20:55:24 +0000

    Tweak address_join method

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,6 +4,6 @@ class Address < ApplicationRecord
   enum address_type: %i[primary previous]
   
   def full_address
-    [line_1, line_2, line_3, post_town, postcode].reject(&:empty?).join(',')
+    [line_1, line_2, line_3, post_town, postcode].reject(&:blank?).join(', ')
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '#full_address' do
+    let(:address) do
+      Fabricate(:address, line_1: '123 Fake Street', line_2: '', line_3: '', post_town: 'Faketown', postcode: 'FAKE123')
+    end
+    
+    it 'joins address elements' do
+      expect(address.full_address).to eq('123 Fake Street, Faketown, FAKE123')
+    end
+  end
+  
 end
