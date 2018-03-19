e107ba86263cb7ce3b2357b0f549bc1f07adb8f3
pezholio <pezholio@gmail.com>
2018-03-19 13:57:57 +0000

    Keep Rubocop happy

--- app/rules/adults_living_elsewhere_rule.rb
+++ app/rules/adults_living_elsewhere_rule.rb
@@ -2,8 +2,12 @@ class AdultsLivingElsewhereRule < Incredible::Rule
   include Rails.application.routes.url_helpers
 
   def process
-    @next_step = :pets if params['application']['adults_living_elsewhere'] == '0'
-    @redirect = new_application_adults_elsewhere_path(application_id: application.id) if params['application']['adults_living_elsewhere'] == '1'
+    @next_step = :pets if adults_living_elsewhere == '0'
+    @redirect = new_application_adults_elsewhere_path(application_id: application.id) if adults_living_elsewhere == '1'
+  end
+  
+  def adults_living_elsewhere
+    params['application']['adults_living_elsewhere']
   end
   
   def application
