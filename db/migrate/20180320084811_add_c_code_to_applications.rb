c71674bfcc1817c1d5c3db2aa81084b6d54d1058
pezholio <pezholio@gmail.com>
2018-03-20 09:23:42 +0000

    Index on code too

--- db/migrate/20180320084811_add_c_code_to_applications.rb
+++ db/migrate/20180320084811_add_c_code_to_applications.rb
@@ -1,6 +1,6 @@
 class AddCCodeToApplications < ActiveRecord::Migration[5.1]
   def change
-    add_column :applications, :code, :string
+    add_column :applications, :code, :string, index: true
     Application.all.each { |a| a.send(:generate_code) }
   end
 end
