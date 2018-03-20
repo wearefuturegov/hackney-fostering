a47521e7c98192ff85d942f9218f747a0bb3c4be
pezholio <pezholio@gmail.com>
2018-03-20 21:15:33 +0000

    Redirect from family form to legal form

--- app/rules/breed_pets_rule.rb
+++ app/rules/breed_pets_rule.rb
@@ -1,7 +1,13 @@
 class BreedPetsRule < Incredible::Rule
-  
+  include Rails.application.routes.url_helpers
+
   def process
-    @next_step = params[:application][:breed_pets] == '1' ? :where_breed_pets : :dunno
+    @next_step = :where_breed_pets if params[:application][:breed_pets] == '1'
+    @redirect = new_application_legal_history_path(application_id: application.id) if params[:application][:breed_pets] == '0'
+  end
+  
+  def application
+    Application.friendly.find(params[:application_id])
   end
   
 end
---
+++ spec/features/rules/breeding_other.feature
@@ -0,0 +1,9 @@
+Feature: Breeding Pets
+
+  Background:
+    Given I have started an application
+    And I am answering the breeding other step of the you and your family form
+    
+  Scenario: Yes redirects correctly
+    And I fill in the 'application_breeding_other' field
+    Then I should be on the 'intro' step
