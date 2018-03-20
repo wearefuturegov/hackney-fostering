cddfc1b62fb2371bd257ccf476e9d2523d2b1cfb
pezholio <pezholio@gmail.com>
2018-03-20 21:03:52 +0000

    Make addresses reusable

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,6 +2,10 @@ module FormHelper
   def setup_application(application)
     application.applicant ||= Person.new
     application.address ||= Address.new
+    application.agency_address ||= Address.new
+    application.other_agency_address ||= Address.new
+    application.previous_agency_address ||= Address.new
+
     application.addresses.build if application.addresses.blank?
     application
   end
