56103f35b3c8913cadbb02f2e14a6a99794bbda6
pezholio <pezholio@gmail.com>
2018-03-20 09:21:30 +0000

    Generate code for existing applications

--- db/migrate/20180320084811_add_c_code_to_applications.rb
+++ db/migrate/20180320084811_add_c_code_to_applications.rb
@@ -1,5 +1,6 @@
 class AddCCodeToApplications < ActiveRecord::Migration[5.1]
   def change
     add_column :applications, :code, :string
+    Application.all.each { |a| a.send(:generate_code) }
   end
 end
