2dc00d8f83574972fb2cf902aa885d5563b78088
pezholio <pezholio@gmail.com>
2018-03-21 12:50:18 +0000

    Make sure names are different each time

--- spec/fabricators/person_fabricator.rb
+++ spec/fabricators/person_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:person) do
-  first_name    FFaker::Name.first_name
-  last_name     FFaker::Name.last_name
+  first_name    { FFaker::Name.first_name }
+  last_name     { FFaker::Name.last_name }
   gender        { Person.genders.values.sample }
   relationship  { Person.relationships.values.sample }
   date_of_birth Date.today - 30.years
