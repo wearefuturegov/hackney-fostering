e5afa673519c45b21d3c4566722b6c4623920fb3
pezholio <pezholio@gmail.com>
2018-03-21 15:11:11 +0000

    Redirect from support carer to consent

---
+++ app/rules/support_carer_email_rule.rb
@@ -0,0 +1,12 @@
+class SupportCarerEmailRule < Incredible::Rule
+  include Rails.application.routes.url_helpers
+
+  def process
+    @redirect = consent_application_path(application)
+  end
+  
+  def application
+    Application.friendly.find(params[:application_id])
+  end
+  
+end
