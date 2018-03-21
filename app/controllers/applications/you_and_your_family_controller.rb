dd52bca173b0279615f53e3a8ccc9dc6617b3294
pezholio <pezholio@gmail.com>
2018-03-21 20:41:57 +0000

    Add missing fields to permitted params

--- app/controllers/applications/you_and_your_family_controller.rb
+++ app/controllers/applications/you_and_your_family_controller.rb
@@ -6,9 +6,21 @@ module Applications
     
     private
     
-    def permitted_params
+    def permitted_params # rubocop:disable Metrics/MethodLength
       params.require(:application).permit(
         :name_change,
+        :primary_language,
+        :other_language,
+        :secondary_languages,
+        :religious,
+        :religion,
+        :practicing,
+        :employment_type,
+        :ni_number,
+        :occupation,
+        :employer,
+        :employer_contact_name,
+        :employer_phone_number,
         :children_at_home,
         :number_of_children,
         :children_elsewhere,
@@ -19,9 +31,31 @@ module Applications
         :number_of_adults_elsewhere,
         :have_pets,
         :number_of_pets,
-        address_attributes: %i[line_1 line_2 line_3 post_town postcode date_from],
-        addresses_attributes: %i[line_1 line_2 line_3 post_town postcode date_from date_to]
+        :breed_pets,
+        :where_breed_pets,
+        :offspring_kept,
+        :breeding_risks,
+        :breeding_other,
+        applicant_attributes: applicant_attributes,
+        address_attributes: address_attributes,
+        addresses_attributes: address_attributes + %i[date_to]
       )
+    end
+    
+    def applicant_attributes
+      %i[
+        previous_first_name
+        previous_last_name
+        date_changed gender
+        date_of_birth
+        country_of_birth
+        town_of_birth
+        nationality
+      ]
+    end
+    
+    def address_attributes
+      %i[line_1 line_2 line_3 post_town postcode date_from]
     end
     
   end
