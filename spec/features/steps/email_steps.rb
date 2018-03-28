0a74fc2bb25e2f8451a9c483cb7b21b2bf1e0cc6
pezholio <pezholio@gmail.com>
2018-03-28 14:44:32 +0100

    Use env var for email

--- spec/features/steps/email_steps.rb
+++ spec/features/steps/email_steps.rb
@@ -6,7 +6,7 @@ module EmailSteps
   end
   
   step 'the fostering team should recieve an eligible email' do
-    should_receive_email('fostering@hackney.gov.uk')
+    should_receive_email(ENV['FOSTERING_EMAIL_ADDRESS'])
     expect(current_email.subject).to eq('New eligibility enquiry')
     expect(current_email.body.raw_source).to match(
       'Someone has filled in an eligibility checker on the Hackney Fostering website'
@@ -14,13 +14,13 @@ module EmailSteps
   end
   
   step 'the fostering team should recieve an ineligible email' do
-    should_receive_email('fostering@hackney.gov.uk')
+    should_receive_email(ENV['FOSTERING_EMAIL_ADDRESS'])
     expect(current_email.subject).to eq('New eligibility enquiry')
     expect(current_email.body.raw_source).to match('They were not eligible to foster')
   end
   
   step 'the fostering team should recieve an application email' do
-    should_receive_email('fostering@hackney.gov.uk')
+    should_receive_email(ENV['FOSTERING_EMAIL_ADDRESS'])
     expect(current_email.subject).to eq('New fostering application')
   end
   
