0655a5382711e8986d3c2ae1496bcb23d30322cf
pezholio <pezholio@gmail.com>
2018-03-28 11:45:19 +0100

    Redirect family court to correct step

--- app/rules/family_court_rule.rb
+++ app/rules/family_court_rule.rb
@@ -1,5 +1,5 @@
 class FamilyCourtRule < Incredible::Rule
   def process
-    @next_step = params[:application][:family_court] == '1' ? :application_date : :previous_applications
+    @next_step = params[:application][:family_court] == '1' ? :court_date : :previous_applications
   end
 end
