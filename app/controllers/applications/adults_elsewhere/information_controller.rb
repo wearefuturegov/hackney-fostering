991072550a0d61dc8c1a9572377be7a9e9021381
pezholio <pezholio@gmail.com>
2018-03-28 21:34:42 +0100

    Use new classes in controllers

--- app/controllers/applications/adults_elsewhere/information_controller.rb
+++ app/controllers/applications/adults_elsewhere/information_controller.rb
@@ -5,6 +5,18 @@ module Applications
       expose :person, -> { application.adults_elsewhere.find(params[:adults_elsewhere_id]) }
       
       form 'adults'
+      
+      private
+      
+      def permitted_params
+        params.require(:adult_elsewhere).permit(
+          :first_name,
+          :last_name,
+          :gender,
+          :date_of_birth,
+          :relationship
+        )
+      end
     end
   end
 end
--- app/controllers/applications/children_elsewhere/information_controller.rb
+++ app/controllers/applications/children_elsewhere/information_controller.rb
@@ -5,6 +5,20 @@ module Applications
       expose :person, -> { application.children_elsewhere.find(params[:children_elsewhere_id]) }
       
       form 'children'
+      
+      private
+      
+      def permitted_params
+        params.require(:child_elsewhere).permit(
+          :first_name,
+          :last_name,
+          :gender,
+          :date_of_birth,
+          :relationship,
+          :school,
+          :school_contact
+        )
+      end
     end
   end
 end
