24d1d5113aeb821bd3f3863515fbb1b52f72edc9
pezholio <pezholio@gmail.com>
2018-03-28 11:47:41 +0100

    Allow mailers to be previewed in development mode

---
+++ app/mailers/previews/applications_preview.rb
@@ -0,0 +1,9 @@
+class ApplicationsPreview < ActionMailer::Preview
+  def eligibility
+    ApplicationsMailer.eligibility(Application.last.code)
+  end
+  
+  def application
+    ApplicationsMailer.application(Application.last.code)
+  end
+end
