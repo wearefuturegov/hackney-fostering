0bbbef799745f8b9f6ac43f61f6ddb27315685a6
pezholio <pezholio@gmail.com>
2018-03-29 11:31:33 +0100

    Remove old relations from person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,12 +1,4 @@
 class Person < ApplicationRecord
-  belongs_to :application, optional: true
-  belongs_to :address, optional: true
-  
-  accepts_nested_attributes_for :address
-  
-  has_many :applications_children
-  has_many :applications_adults
-
   enum gender: %i[male female other],
        nationality: %i[british british_english british_scottish british_welsh],
        relationship: %i[child sibling foster_child stepchild other_relationship]
