dae65c42564d4eba3d7bee564f2edc0f477624e0
pezholio <pezholio@gmail.com>
2018-04-03 21:26:13 +0100

    Update consent tests

--- spec/features/steps/consent_steps.rb
+++ spec/features/steps/consent_steps.rb
@@ -1,10 +1,7 @@
 module ConsentSteps
   
   step 'I consent and sign the form' do
-    find(:css, '#application_agree_to_checks').set(true)
-    find(:css, '#application_agree_to_la_contact').set(true)
-    fill_in 'application_signature', with: 'My Name'
-    click_on 'Consent and Submit'
+    click_on I18n.t('continue')
   end
   
 end
