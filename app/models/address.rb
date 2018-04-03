b298c4f089aac71d1dda774dab4809f0e5a07718
pezholio <pezholio@gmail.com>
2018-04-03 11:08:45 +0100

    Add rules for addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,12 @@
 class Address < ApplicationRecord
-  belongs_to :application, optional: true
+  has_one :application
   
   enum address_type: %i[primary previous]
   
+  validates :line_1, presence: true, if: -> { application&.on_step?(%w[full_address reference_address]) }
+  validates :post_town, presence: true, if: -> { application&.on_step?(%w[full_address reference_address]) }
   validates :postcode, presence: true
+  validates :date_from, presence: true, if: -> { application&.on_step?(%w[full_address]) }
   
   def full_address
     [line_1, line_2, line_3, post_town, postcode].reject(&:blank?).join(', ')
