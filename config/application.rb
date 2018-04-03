a886f532273c50239820857289a450eb8f787ab6
pezholio <pezholio@gmail.com>
2018-04-03 09:42:08 +0100

    Add validations for application

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,10 @@ module HackneyFostering
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     config.autoload_paths << Rails.root.join('app', 'models', 'people').to_s
     config.autoload_paths << Rails.root.join('app', 'lib').to_s
+    config.autoload_paths += [config.root.join('app')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
