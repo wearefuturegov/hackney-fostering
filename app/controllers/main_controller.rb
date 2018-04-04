e4e05b703fde33d3c82137b0d49a7ae43f45c914
pezholio <pezholio@gmail.com>
2018-04-04 17:15:36 +0100

    Force reload when navigating back

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_current_path, :set_current_step, only: [:show]
+  before_action :set_cache_headers, :set_current_path, :set_current_step, only: [:show]
   
   def index; end
   
@@ -18,4 +18,12 @@ class MainController < ActionController::Base
     @valid
   end
   
+  private
+  
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+  end
+    
 end
