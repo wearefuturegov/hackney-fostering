c8b162797c2016c6be115b08d29c4bbad0d66f89
pezholio <pezholio@gmail.com>
2018-04-05 12:15:37 +0100

    Delete pet subform

--- app/rules/conflict_rule.rb
+++
@@ -1,9 +0,0 @@
-class ConflictRule < ApplicationRule
-  def process
-    @redirect = if application.pets.count == application.number_of_pets
-                  application_you_and_your_family_path(application_id: application.id, id: :breed_pets)
-                else
-                  new_application_pet_path(application_id: application.id)
-                end
-  end
-end
