302e10bbe7bb99b41429973a3521e93947246be0
pezholio <pezholio@gmail.com>
2018-04-16 13:48:48 +0100

    Hide and show inputs, rather than add classes

--- app/assets/javascripts/show_hide_extra.js
+++ app/assets/javascripts/show_hide_extra.js
@@ -3,11 +3,11 @@ $.fn.showHideExtra = function() {
   if (this.length === 0) { return false; }
     
   $(this).find('input[value=1]').click(function(){
-      $('.extra').removeClass('hidden');
+      $('.extra').show();
   });
   
   $(this).find('input[value=0]').click(function(){
-      $('.extra').addClass('hidden');
+      $('.extra').hide();
   });
 
 }
