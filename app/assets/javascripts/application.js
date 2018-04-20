8db991c5aa52647eff372456df142eee648d45f8
Chris Evans <evanschris@users.noreply.github.com>
2018-04-20 17:19:52 +0100

    add in consent content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require geff/alerts
+//= require geff/accordion
 //= require getaddress
 //= require delete_address
 //= require show_hide_extra
--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -5,15 +5,11 @@ $(document).ready(function() {
   $('.previous_addresses').getAddress();
   $('.referee_form').getAddress();
   $('.delete_address').deleteAddress();
-  // $('.application_children_at_home').showHideExtra();
-  // $('.application_children_living_elsewhere').showHideExtra();
-  // $('.application_adults_living_at_home').showHideExtra();
-  // $('.application_adults_living_elsewhere').showHideExtra();
   $('.application_have_pets').showHideExtra();
   $('header .menu_btn').menuToggle();
+  $('.accordion').each(function() { $(this).accordion() });
 
   $('.disabled a').click(function(e) {
-    console.log('test');
     e.preventDefault();
   });
 });
